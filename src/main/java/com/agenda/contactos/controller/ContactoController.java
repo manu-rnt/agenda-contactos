e805d704635e9124ba110b9a3d708181334a4720
ManuVillarba <manuelvillarba@gmail.com>
2022-09-14 00:37:03 -0300

    beta

--- src/main/java/com/agenda/contactos/controller/ContactoController.java
+++ src/main/java/com/agenda/contactos/controller/ContactoController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -47,4 +49,39 @@ public class ContactoController {
 		return "redirect:/";
 	}
 	
+	@GetMapping("/{id}/edit")
+	public String updateShowRegisterContactForm (@PathVariable Integer id, Model model) {
+		Contacto contacto = contactoRepository.getById(id);
+		model.addAttribute("contacto", contacto);
+		return "new";
+	}
+	
+	@PostMapping("/{id}/edit")
+	public String updateContact(@PathVariable Integer id, @Validated Contacto contacto, BindingResult bindingResult, RedirectAttributes redirect, Model modelo) {
+		
+		Contacto contactoDB = contactoRepository.getById(id);
+		
+		if (bindingResult.hasErrors()) {
+			modelo.addAttribute("contacto", contacto);
+			return "new";
+		}
+		
+		contactoDB.setName(contacto.getName());
+		contactoDB.setPhoneNumber(contacto.getPhoneNumber());
+		contactoDB.setEmail(contacto.getEmail());
+		contactoDB.setBirthDate(contacto.getBirthDate());
+		
+		
+		contactoRepository.save(contactoDB);
+		redirect.addFlashAttribute("msgExito", "El contacto ha sido actualizado correctamente");
+		return "redirect:/";
+	}
+	
+	@PostMapping("/{id}/delete")
+	public String deleteContact(@PathVariable Integer id, RedirectAttributes redirect) {
+		Contacto contacto = contactoRepository.getById(id);
+		contactoRepository.delete(contacto);
+		redirect.addFlashAttribute("msgExito", "El contacto ha eliminado correctamente");
+		return "redirect:/";
+	}
 }
